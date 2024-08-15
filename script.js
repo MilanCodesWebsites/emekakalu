1343c582a404b36410b721d93c4a1231fe3e92a6
Al Titan <bullionfx2@proton.me>
2024-08-15 23:35:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -100,3 +100,11 @@ document.addEventListener("DOMContentLoaded", function () {
   startAutoSlide();
   initApp();
 });
+
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
+});
